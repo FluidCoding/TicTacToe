0b65b912450f3ce56fd412975bad76a4edaf7d57
FluidCoding <sonnie130@gmail.com>
2016-01-16 22:34:47 -0500

    Win Check and Rematch

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,20 @@
-var fbRef, id, player;
+var fbRef, id, player, inGame;
 $(function(){
   fbRef = new Firebase("https://fluidcode-tictactoe.firebaseio.com/games/");
+  inGame = false;
   fbRef.on("child_added", listGames);
 });
 
 function joinGame(){
   console.log("Joined Game!");
+  inGame = true;
   $("#btnCreateGame").hide();
   $("#welcome").hide();
 }
 
 function joinGameByID(gameId){
   fbRef.off("child_added", listGames);
+  inGame = true;
   id = new Firebase("https://fluidcode-tictactoe.firebaseio.com/games/-" + gameId);
   player = 'O';
   id.child("players").set(2);
@@ -20,6 +23,7 @@ function joinGameByID(gameId){
 
 function createGame(){
   fbRef.off("child_added", listGames);
+  inGame = true;
   if(fbRef!==undefined){
     id = fbRef.push(Game, joinGame);
     player = 'X';
@@ -28,18 +32,19 @@ function createGame(){
 }
 
 function rematch(){
-    if(!Game.isOver){
+    if(Game.isOver){
       Game.isOver = false;
       Game.turn = 'X';
       Game.players = 2;
       Game.tiles = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
       id.set(Game, function(Error){console.log(Error);});
+      $("#btnRematch").remove();
     }
 }
 
 // TODO: Enforce Turns
 function selectToe(row, col){
-  console.log(Game.turn == player);
+  if(Game.isOver)return;
   if(Game.players===2 && Game.turn == player){
     console.log("ROW " + row + " COL " + col);
     if(Game.tiles[row][col]===' '){
@@ -50,21 +55,43 @@ function selectToe(row, col){
     }
   }
 }
+
 function clearGames(){
   fbRef.set(null,  function(Error){console.log(Error);});
 }
+
 function checkWin(){
   var winner = -1;
   //Check horizontals
   for(var row=0;row<3;row++){
-    for(var )
+    if(Game.tiles[row][0] !== ' ' && Game.tiles[row][0]===Game.tiles[row][1] && Game.tiles[row][0]===Game.tiles[row][2]){
+       return (Game.tiles[row][0] === 'X'? 1 : 2);
+    }
   }
   // Check Verticals
-
+  for(var col=0;col<3; col++){
+    if(Game.tiles[0][col] !== ' ' && Game.tiles[0][col] === Game.tiles[1][col] && Game.tiles[0][col] === Game.tiles[2][col]){
+      return (Game.tiles[0][col] === 'X'? 1 : 2);
+    }
+  }
   // Check Diagnols
+  if(Game.tiles[0][0] !== ' ' && Game.tiles[0][0] === Game.tiles[1][1] && Game.tiles[0][0] === Game.tiles[2][2])
+    return (Game.tiles[1][1] == 'X'? 1 : 2);
+  if(Game.tiles[0][2] !== ' ' && Game.tiles[0][2] === Game.tiles[1][1] && Game.tiles[0][2] === Game.tiles[2][0])
+    return (Game.tiles[1][1] === 'X'? 1 : 2);
 
   // Check Tie
+  var emptyTiles = 0;
+  for(var row=0; row<3; row++){
+    for(var col=0; col<3; col++){
+      if(Game.tiles[row][col] === ' ')
+        emptyTiles++;
+    }
+  }
+  if(emptyTiles===0)
+    winner = 0;
 
+  return winner;
 }
 function displayGame(snapshot){
   if(snapshot===null)return;
@@ -73,16 +100,37 @@ function displayGame(snapshot){
   Game = snapshot.val();
   // TODO: Check for Win her
   var winner = checkWin();
+  console.log("Win Check "+ winner);
   var spacer = "______";
   var board = $("#board");
   var cached = [];
   board.empty();
-  if(Game.players===2){
-    cached.push("<p>" + Game.turn + "'s Turn</p>");
+  switch(winner){
+  case -1:
+    if(Game.players===2){
+      cached.push("<p>" + Game.turn + "'s Turn</p>");
+    }
+    else {
+        cached.push("<p> Waiting for player 2 to join...</p>");
+    }
+    break;
+  case 0: // Tie
+    cached.push("<p> Tie Game...</p>");
+    cached.push("<button id=\"btnRematch\" onclick=\"rematch()\" type=\"button\" class=\"btn btn-success\">Rematch</button><br>");
+    Game.isOver = true;
+    break;
+  case 1: // X
+    cached.push("<p> X Wins!!! </p>");
+    cached.push("<button id=\"btnRematch\" onclick=\"rematch()\" type=\"button\" class=\"btn btn-success\">Rematch</button><br>");
+    Game.isOver = true;
+    break;
+  case 2: // O
+    cached.push("<p> O Wins!!! </p>");
+    cached.push("<button id=\"btnRematch\" onclick=\"rematch()\" type=\"button\" class=\"btn btn-success\">Rematch</button><br>");
+    Game.isOver = true;
+    break;
   }
-  else {
-    cached.push("<p> Waiting for player 2 to join...</p>");
-  }
+
   for(var row = 0; row<3; row++){
     for(var col=0; col<3; col++){
       cached.push("<button onclick=\"selectToe("+row+" , " + col+")\" id=\"tile\" type=\"button\" class=\"btn btn-secondary\"> "+ Game.tiles[row][col] + " </button>");
@@ -97,16 +145,19 @@ function displayGame(snapshot){
   }
   board.append(cached.join(""));
 }
-var count = 0;
+
+var gamesCount = 0;
 function listGames(snapshot){
   if(snapshot===null)return;
-  count++;
-  console.log(snapshot.val());
-  var board = $("#board");
-  var gameId = String(snapshot.key()).slice(1);
-  var cached = [];
-  cached.push("<br>  <button id=\"btnJoinGame\" onclick=\"joinGameByID(\'"+ gameId + "\')\" type=\"button\" class=\"btn btn-success\">Join Game</button><br>");
-  board.append(cached.join(""));
+  if(snapshot.val().players === 1){
+    gamesCount++;
+    console.log(snapshot.val());
+    var board = $("#board");
+    var gameId = String(snapshot.key()).slice(1);
+    var cached = [];
+    cached.push("<br>  <button id=\"btnJoinGame\" onclick=\"joinGameByID(\'"+ gameId + "\')\" type=\"button\" class=\"btn btn-success\">Join Game</button><br>");
+    board.append(cached.join(""));
+  }
 }
 
 var Game = {
